153c967c2aa97c842fe846654d3131271f2b436c
Mayank Baranwal <baranwalm2002@gmail.com>
2019-02-06 23:13:56 +0530

    REbuild

--- Assigment/Assigment/BooksIssued.Designer.vb
+++ Assigment/Assigment/BooksIssued.Designer.vb
@@ -22,8 +22,16 @@ Partial Class BooksIssued
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.SuspendLayout()
+        '
+        'BooksIssued
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.Name = "BooksIssued"
+        Me.Size = New System.Drawing.Size(852, 700)
+        Me.ResumeLayout(False)
+
     End Sub
 
 End Class
